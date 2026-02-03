588c297ff61aa3d833f29ded0b8f4a7daa582940
apebnb <128845747+musterman7@users.noreply.github.com>
2026-02-03 11:41:48 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
+export function middleware(req: NextRequest) {
   return NextResponse.redirect(
-    'https://four.meme?code=C5UE2M5H89FC'
-  );
+    new URL('https://four.meme?code=C5UE2M5H89FC')
+  )
 }
 
 export const config = {
-  matcher: '/',
-};
+  matcher: '/:path*',
+}
