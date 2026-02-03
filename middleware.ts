f940f605e30fce9eef341fdd0f640c6cbe9053bf
apebnb <128845747+musterman7@users.noreply.github.com>
2026-02-03 11:39:16 +0700

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(
+    'https://four.meme?code=C5UE2M5H89FC'
+  );
+}
+
+export const config = {
+  matcher: '/',
+};
